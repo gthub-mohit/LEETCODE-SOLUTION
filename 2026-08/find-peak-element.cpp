2241b4bb8a04784b5a005fc0fb876490ffec0313
Mohit <162956586+gthub-mohit@users.noreply.github.com>
2026-08-13 20:22:06 +0530

    Solved find-peak-element on LeetCode [C++]

---
+++ 2026-08/find-peak-element.cpp
@@ -0,0 +1,28 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : find-peak-element                                           ║
+ ║  Platform : LeetCode                                                    ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 13, 2026                                             ║
+ ║  URL      : https://leetcode.com/problems/find-peak-element/submissions/2105560573/║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();;
+        int low=1;
+        int high=n-2;
+        if(n==1)return 0;
+        if(nums[0]>nums[1])return 0;
+        if(nums[n-1]>nums[n-2])return n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])return mid;
+            if(nums[mid]>nums[mid+1])high=mid-1;
+            else low=mid+1;
+        }
+        return -1;
+    }
+};
