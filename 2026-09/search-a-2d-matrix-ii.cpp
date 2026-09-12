5c4ecd09425bc979bd398a9ed7bb40c047255959
Mohit <162956586+gthub-mohit@users.noreply.github.com>
2026-09-12 12:50:01 +0530

    Solved search-a-2d-matrix-ii on LeetCode [C++]

--- 2026-09/search-a-2d-matrix-ii.cpp
+++ 2026-09/search-a-2d-matrix-ii.cpp
@@ -4,29 +4,21 @@
  ║  Platform : LeetCode                                                    ║
  ║  Status   : Accepted                                                    ║
  ║  Date     : September 12, 2026                                          ║
- ║  URL      : https://leetcode.com/problems/search-a-2d-matrix-ii/submissions/2139245567/║
+ ║  URL      : https://leetcode.com/problems/search-a-2d-matrix-ii/        ║
  ╚═══════════════════════════════════════════════════════════════════════╝
  */
 
 class Solution {
 public:
-    bool binarysearch(vector<int>&arr , int n , int target){
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==target)return true;
-            else if(arr[mid]>target)high=mid-1;
-            else low=mid+1;
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        for(int k=0;k<n;k++){
-            if(target>=matrix[k][0]&& target<=matrix[k][m-1])
-            if(binarysearch(matrix[k] , m ,target))return true;
+        int row=0;
+        int column=m-1;
+        while(row<n && column>=0){
+            if(matrix[row][column]==target)return true;
+            else if(matrix[row][column]>target)column--;
+            else row++;
         }
         return false;
     }
